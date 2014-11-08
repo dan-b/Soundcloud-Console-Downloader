b58375b89771470cbae5df58e0876f3dbc974499
dan-b <danbendelic@gmail.com>
2014-11-08 18:40:10 -0500

    Added tests

--- SoundcloudDownloader_Console/SoundcloudClient.cs
+++ SoundcloudDownloader_Console/SoundcloudClient.cs
@@ -7,9 +7,9 @@ using System.Net;
 using System.IO;
 using System.Text.RegularExpressions;
 
-namespace SoundcloudDownloader_Console
+namespace Soundcloud
 {
-    class Program
+    class SoundcloudClient
     {
         static String clientID = "b45b1aa10f1ac2941910a7f0d10f8e28";
 
@@ -51,14 +51,26 @@ namespace SoundcloudDownloader_Console
         /// </summary>
         /// <param name="url"></param>
         /// <returns></returns>
-        public static TrackID resolveTrackID(String url, int startindex)
+        public static TrackID ResolveTrackID(String url, int startindex)
         {
             WebClient w = new WebClient();
-            String str = w.DownloadString("http://api.soundcloud.com/resolve.json?url=" + url + "&client_id=" + clientID);
+            //String str = w.DownloadString("http://api.soundcloud.com/resolve.json?url=" + url + "&client_id=" + clientID);
+            String str =
+                "{\"kind\":\"track\",\"id\":15993727,\"created_at\":\"2011/05/26 20:00:30 +0000\",\"user_id\":2176409,\"duration\":145165,\"commentable\":true,\"state\":\"finished\",\"original_content_size\":25585056,\"last_modified\":\"2014/10/25 19:47:30 +0000\",\"sharing\":\"public\",\"tag_list\":\"\",\"permalink\":\"finally-remix\",\"streamable\":true,\"embeddable_by\":\"all\",\"downloadable\":false,\"purchase_url\":\"http://www.beatport.com/track/finally-mord-fustang-remix/2749900\",\"label_id\":null,\"purchase_title\":null,\"genre\":\"\",\"title\":\"Froidz - Finally (Mord Fustang Remix)\",\"description\":\"\",\"label_name\":\"Yawa Recordings\",\"release\":\"\",\"track_type\":\"remix\",\"key_signature\":\"\",\"isrc\":\"\",\"video_url\":null,\"bpm\":null,\"release_year\":2011,\"release_month\":8,\"release_day\":30,\"original_format\":\"wav\",\"license\":\"all-rights-reserved\",\"uri\":\"https://api.soundcloud.com/tracks/15993727\",\"user\":{\"id\":2176409,\"kind\":\"user\",\"permalink\":\"mordfustang\",\"username\":\"Mord Fustang\",\"last_modified\":\"2014/10/13 13:54:42 +0000\",\"uri\":\"https://api.soundcloud.com/users/2176409\",\"permalink_url\":\"http://soundcloud.com/mordfustang\",\"avatar_url\":\"https://i1.sndcdn.com/avatars-000079911702-qfflx7-large.jpg\"},\"permalink_url\":\"http://soundcloud.com/mordfustang/finally-remix\",\"artwork_url\":\"https://i1.sndcdn.com/artworks-000009997346-4ts29q-large.jpg\",\"waveform_url\":\"https://w1.sndcdn.com/qhRMZDL3bHd3_m.png\",\"stream_url\":\"https://api.soundcloud.com/tracks/15993727/stream\",\"playback_count\":281965,\"download_count\":0,\"favoritings_count\":3214,\"comment_count\":283,\"likes_count\":3214,\"reposts_count\":308,\"attachments_uri\":\"https://api.soundcloud.com/tracks/15993727/attachments\",\"policy\":\"ALLOW\"}";
+            try
+            {
+                dynamic track = Newtonsoft.Json.JsonConvert.DeserializeObject(str);
+                var id = track.id;
+            }
+            catch (Exception exception)
+            {
+                var message = exception.Message;
+            }
             //int index = str.IndexOf("\"id\":", startindex) + 5;
             int index = str.IndexOf("\"track\",\"id\":", startindex) + 13;
             int titleindex = str.IndexOf("\"title\":", index) + 9;
-            return new TrackID(index, str.Substring(index, str.IndexOf(",", index) - index), str.Substring(titleindex, str.IndexOf("\",", titleindex) - titleindex));
+            TrackID trackID = new TrackID(index, str.Substring(index, str.IndexOf(",", index) - index), str.Substring(titleindex, str.IndexOf("\",", titleindex) - titleindex));
+            return trackID;
         }
 
         /// <summary>
@@ -74,7 +86,7 @@ namespace SoundcloudDownloader_Console
             int currentindex = 0;
             for (int i = 0; i < getTrackCount(url); i++)
             {
-                TrackID t = resolveTrackID(url, currentindex);
+                TrackID t = ResolveTrackID(url, currentindex);
                 currentindex = t.index + 1;
                 ret.Add(t);
             }
@@ -108,7 +120,7 @@ namespace SoundcloudDownloader_Console
         /// <param name="url"></param>
         public static void downloadSong(String url)
         {
-            TrackID trackID = resolveTrackID(url, 0);
+            TrackID trackID = ResolveTrackID(url, 0);
             String downloadurl = resolveDownloadURL(trackID.id).Replace("\\u0026", "&");
             WebClient w = new WebClient();
             String filename = trackID.title + ".mp3";
---
+++ SoundcloudDownloader_Console/SoundcloudClientTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NUnit.Framework;
+using Newtonsoft.Json;
+
+namespace Soundcloud
+{
+    class SoundcloudClientTests
+    {
+        [Test]
+        [TestCase(@"https://soundcloud.com/mordfustang/finally-remix", 0)]
+        public void ResolveTrackID(String url, int startindex)
+        {
+            var TrackID = SoundcloudClient.ResolveTrackID(url, startindex);
+        }
+    }
+}
